3841e9d987388c1874d8b110e22290ee3ddfb018
PMSchultz <bdauburn@comcast.net>
2017-10-11 00:46:26 -0600

    First attempt at Padovan Sequence

---
+++ src/edu/cnm/deepdive/padovan/Padovan.java
@@ -0,0 +1,45 @@
+package edu.cnm.deepdive.padovan;
+
+import java.math.BigInteger;
+
+
+public class Padovan implements Iterable<BigInteger> {
+
+  private final int numTerms;
+
+  public Padovan(int numTerms) {
+    this.numTerms = numTerms;
+  }
+
+  @Override
+  public java.util.Iterator<BigInteger> iterator() {
+
+    return new Iterator();
+  }
+
+  private class Iterator implements java.util.Iterator<BigInteger> {
+
+    private int currentTerm = 0;
+    private BigInteger p0 = BigInteger.ZERO;
+    private BigInteger p1 = BigInteger.ZERO;
+    private BigInteger p2 = BigInteger.ONE;
+
+    @Override
+    public boolean hasNext() {
+      return (currentTerm < numTerms);
+    }
+
+    @Override
+    public BigInteger next() {
+      BigInteger next = p0.add(p1);
+      p0 = p1;
+      p1 = p2;
+      p2 = next;
+      currentTerm++;
+      return next;
+    }
+
+  }
+}
+
+
